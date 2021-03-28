98a45f93355bd7426e9d003d8928f6420ede3435
Ronald <rdajila@gmail.com>
2021-03-28 18:47:40 +0200

    examples of IN && ::

--- statement.py
+++ statement.py
@@ -1,17 +1,3 @@
-# score = input("Enter Score: ")
-# _value = float(score)
-# if _value < 0.0 or _value > 1.0:
-#     print("Error")
-# elif _value >= 0.9:
-#     print("A")
-# elif _value >= 0.8:
-#     print("B")
-# elif _value >= 0.7:
-#     print("c")
-# elif _value >= 0.6:
-#     print("d")
-# else:
-# 	print("F")
 for _el in range(0, 10, 2):
     print(_el)
 
@@ -24,7 +10,20 @@ print(_notes[-2:1]) # empty
 print(_notes[:-2])
 print(_notes[-2:])
 print(_notes[-4:-2])
-print(_notes[-2:-4]) # empty
-print(_notes[-2:1:-1])
+print('_notes[-2:-4] -> ', _notes[-2:-4]) # empty
+print('_notes[-2:1:-1] -> ', _notes[-2:1:-1])
 print(_notes[::1])
 print(_notes[::2])
+
+listName = ['Ronald', 'Daniel']
+print('\'Ronald\' in list: ', 'Ronald' in listName) # TRUE
+print('\'Ron\' in list: ', 'Ron' in listName) # FALSE, because evaluate all content of element
+
+txtName = 'Ronald Daniel'
+print('\'Ronald\' in str: ', 'Ronald' in txtName) # TRUE
+print('\'Ron\' in str: ', 'Ron' in txtName) # TRUE, because evaluate if exits of content on str
+
+print(ord('a'))
+
+print(3*'abc'+'zz')
+print(float("1.3"))
