a739b79a8b8a1a46d90c00deb52c383e42e5b87a
Ronald <rdajila@gmail.com>
2021-04-27 20:32:49 +0200

    Check cases slice

---
+++ rodaja.py
@@ -0,0 +1,13 @@
+name = 'RonaldDaniel'
+print(name[:])
+print(name[::1])
+print(name[::-1])
+print(name[-10:])
+print(name[-10:-2])
+print(name[-10:-2:2])
+print(name[-10:-2:-2]) # empty
+print(name[-2:-10]) # empty
+print(name[-2:-10:-1])
+print(name[-2::-1])
+print(name[-2::1])
+print(name[-2:])
